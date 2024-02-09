9539b1d7826e39a350f8a6531bee9e751ce6209a
sashi0034 <satoshi3104.nishi@gmail.com>
2024-02-09 13:15:59 +0900

    トレース用の構造体を定義

---
+++ Fumichou-Nes/src/TraceLog.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "TraceLog.h"
+
+#include "Mos6502Instruction.h"
+
+namespace Nes
+{
+	String TraceMemoryRead::Stringify() const
+	{
+		return U"${} -> {}"_fmt(addr, read);
+	}
+
+	String TraceMemoryWrite::Stringify() const
+	{
+		return U"${} -> {}"_fmt(addr, wrote);
+	}
+
+	String TraceCpuOperation::Stringify() const
+	{
+		return String(GetMos6502Instruction(opcode).desc);
+	}
+}
---
+++ Fumichou-Nes/src/TraceLog.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Forward.h"
+
+namespace Nes
+{
+	struct TraceMemoryRead
+	{
+		addr16 addr;
+		uint8 read;
+
+		String Stringify() const;
+	};
+
+	struct TraceMemoryWrite
+	{
+		addr16 addr;
+		uint8 wrote;
+
+		String Stringify() const;
+	};
+
+	struct TraceCpuOperation
+	{
+		addr16 pc;
+		uint8 opcode;
+
+		String Stringify() const;
+	};
+
+	using TraceLog = std::variant<TraceMemoryRead, TraceMemoryWrite, TraceCpuOperation>;
+}
