507ce9642105a8939fab546d962aab326a462345
sashi0034 <satoshi3104.nishi@gmail.com>
2024-02-11 12:11:03 +0900

    ステータス画面仮追加

---
+++ Fumichou-Gui/Gui/GuiStatus.cpp
@@ -0,0 +1,63 @@
+﻿#include "stdafx.h"
+#include "GuiStatus.h"
+
+#include "FontKeys.h"
+#include "HwFrame.h"
+#include "WidgetDocument.h"
+
+using namespace Gui;
+
+namespace
+{
+	struct CpuView
+	{
+	};
+
+	struct StatusDraw : Document::Drawer
+	{
+		using Drawer::operator();
+
+		void operator ()(CpuView) const
+		{
+			auto&& cpu = Nes::HwFrame::Instance().GetHw().GetMos6502();
+			const String text = U"PC={:04X}"_fmt(cpu.GetRegs().pc);
+			FontAsset(FontKeys::ZxProto_20_Bitmap)(text)
+				.draw(Arg::leftCenter = leftCenter, Palette::Darkgray);
+		}
+	};
+
+	using StatusDocumentData = DocumentData<StatusDraw, CpuView>;
+
+	void generateTexts(StatusDocumentData::array_type& texts)
+	{
+		texts.push_back(Document::HeaderText(U"CPU status"));
+		texts.push_back(std::monostate{});
+		texts.push_back(CpuView());
+	}
+}
+
+struct GuiStatus::Impl
+{
+	WidgetDocument<StatusDocumentData> m_document{};
+
+	void Update(const Size& availableRegion)
+	{
+		const ScopedViewport2D viewport2D{availableRegion};
+		if (not m_document.Data().Size()) generateTexts(m_document.Data().Raw());
+		m_document.Update(availableRegion);
+	}
+};
+
+namespace Gui
+{
+	GuiStatus::GuiStatus()
+		:
+		p_impl(std::make_shared<Impl>())
+	{
+	}
+
+	void GuiStatus::Update(const Size& availableRegion)
+	{
+		p_impl->Update(availableRegion);
+	}
+}
---
+++ Fumichou-Gui/Gui/GuiStatus.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace Gui
+{
+	class GuiStatus
+	{
+	public:
+		GuiStatus();
+		void Update(const Size& availableRegion);
+
+	private:
+		struct Impl;
+		std::shared_ptr<Impl> p_impl;
+	};
+}
