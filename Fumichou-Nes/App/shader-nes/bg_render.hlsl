b9afb6062a29728084da9f3f0e95ac1b08698329
sashi0034 <satoshi3104.nishi@gmail.com>
2024-02-17 18:16:01 +0900

    ピクセルシェーダーからピクセルフェッチする部分まで実装

--- Fumichou-Nes/App/shader-nes/bg_render.hlsl
+++ Fumichou-Nes/App/shader-nes/bg_render.hlsl
@@ -1,6 +1,6 @@
 //	Textures
 //
-Texture2D g_texture0 : register(t0);
+Texture2D g_patternTableTexture : register(t0);
 SamplerState g_sampler0 : register(s0);
 
 namespace s3d
@@ -28,27 +28,43 @@ cbuffer PSConstants2D : register(b0)
     float4 g_internal;
 }
 
-cbuffer CbPaletteColors : register(b1)
+cbuffer CbBgData : register(b1)
 {
-    float4 g_colors[64];
+    uint2 g_patternTableSize;
+    uint4 g_nametable[256]; // 4KiB
+    uint4 g_palettes[2]; // 32B
 }
 
-// cbuffer CbPaletteIndex : register(b2)
-// {
-//     uint4 g_palettes;
-// }
+#define W_256 256
+#define H_240 240
+#define TILE_8 8
+
+// ((array[address / 16][(address & 16) / 4] >> ((address % 4) * 8)) & 0xFF)
+#define FROM_UINT8(array, address) ((array[address >> 4][(address & 0xF) >> 2] >> ((address & 0x3) << 3)) & 0xFF)
 
 float4 PS(s3d::PSInput input) : SV_TARGET
 {
-    float4 color0 = g_texture0.Sample(g_sampler0, input.uv);
+    const uint2 screenPos = input.uv * float2(W_256, H_240);
+    const uint2 tileCoarse = screenPos / TILE_8;
+    const uint2 tileFine = screenPos - tileCoarse * TILE_8;
 
-    const int index = (uint)(color0.x) * 2 + (uint)(color0.y);
-    const uint paletteId =
-        (index == 0) * (input.color.r * 64) +
-        (index == 1) * (input.color.g * 64) +
-        (index == 2) * (input.color.b * 64) +
-        (index == 3) * (input.color.a * 64);
-    color0 = g_colors[paletteId];
+    const uint addr = tileCoarse.x + tileCoarse.y * 32;
 
-    return color0 + g_colorAdd;
+    // const uint16_t addr = 1;
+    // const uint addr = 0;
+    // const uint tileId = ((g_nametable[addr / 16][(addr % 16) / 4]) >> 8) & 0xFF;
+    // const uint tileId = (g_nametable[addr / 16][(addr % 16) / 4] >> ((addr % 4) * 8)) & 0xFF;
+    // const uint tileId = (g_nametable[addr >> 4][(addr & 0xF) >> 2] >> ((addr & 0x3) << 3)) & 0xFF;
+    const uint tileId = FROM_UINT8(g_nametable, addr);
+
+    const float2 tileUV = float2(uint2(tileId * TILE_8, 0) + tileFine) / g_patternTableSize;
+    const float4 tileColor = g_patternTableTexture.Sample(g_sampler0, tileUV);
+
+    float4 outputColor = tileColor;
+
+    // outputColor = g_colors[paletteId];
+    // outputColor.r = tileId / 255.0;
+    // outputColor.g = tileId / 255.0;
+
+    return outputColor + g_colorAdd;
 }
--- Fumichou-Nes/src/Ppu_In_Render.cpp
+++ Fumichou-Nes/src/Ppu_In_Render.cpp
@@ -22,10 +22,9 @@ namespace
 
 	struct CbBgData
 	{
-		uint32 patternTableLength[4];
-		uint32 tileSize[4];
+		uint32 patternTableSize[4];
 		uint32 nametable[4 * 256];
-		uint32 palettes[2 * 32];
+		uint32 palettes[4 * 2];
 	};
 }
 
@@ -59,7 +58,8 @@ public:
 		// BG描画
 		constexpr int w_32 = DisplayWidth_256 / tile_8;
 		constexpr int h_30 = DisplayHeight_240 / tile_8;
-		static ConstantBuffer<CbPaletteIndex> cbPaletteIndex{};
+		// static ConstantBuffer<CbPaletteIndex> cbPaletteIndex{};
+		static ConstantBuffer<CbBgData> cbBgData{};
 
 		static TimeProfiler profiler{U"PPU Rendering"};
 		profiler.begin(U"BG");
@@ -67,36 +67,34 @@ public:
 		// cbBgData->patternTableLength[0] = patternTable.width() / 16;
 		// cbBgData->tileSize[0] = 32;
 		// cbBgData->tileSize[1] = 30;
-		// std::memcpy(&cbBgData->nametable, ppu.m_nametableData.data(), sizeof(cbBgData->nametable));
-		// std::memcpy(&cbBgData->palettes, ppu.m_palettes.data(), sizeof(cbBgData->palettes));
-		// Graphics2D::SetPSConstantBuffer(2, cbBgData);
+		cbBgData->patternTableSize[0] = patternTable.width();
+		cbBgData->patternTableSize[1] = patternTable.height();
+		std::memcpy(&cbBgData->nametable, ppu.m_nametableData.data(), sizeof(cbBgData->nametable));
+		std::memcpy(&cbBgData->palettes, ppu.m_palettes.data(), sizeof(cbBgData->palettes));
+		Graphics2D::SetPSConstantBuffer(1, cbBgData);
 		// Graphics2D::SetPSTexture(0, patternTable);
 		// // Graphics2D::DrawTriangles(w_32 * h_30 * 2);
 		// Graphics2D::DrawTriangles(1);
 
-		for (int16 y = 0; y < h_30; ++y)
-		{
-			for (int16 x = 0; x < w_32; ++x)
-			{
-				const uint16 addr = x + y * 32;
-				const uint8 id = ppu.m_nametableData[addr]; // mmu.ReadChr8(addr);
-				const uint8 attrIndex = 0x3C0 | (addr & 0xC00) | ((addr >> 4) & 0x38) | ((addr >> 2) & 0x7);
-				const uint8 attribute = ppu.m_nametableData[attrIndex];
-				const uint8 shift = ((addr >> 4) & 4) | (addr & 2);
-				const uint8 paletteIndex = ((attribute >> shift) & 0x3) << 2;
-				ColorF color{};
-				color.r = readPalette(ppu, paletteIndex + 0) / 64.0;
-				color.g = readPalette(ppu, paletteIndex + 1) / 64.0;
-				color.b = readPalette(ppu, paletteIndex + 2) / 64.0;
-				color.a = readPalette(ppu, paletteIndex + 3) / 64.0;
-				// cbPaletteIndex->palette[0] = readPalette(ppu, paletteIndex + 0);
-				// cbPaletteIndex->palette[1] = readPalette(ppu, paletteIndex + 1);
-				// cbPaletteIndex->palette[2] = readPalette(ppu, paletteIndex + 2);
-				// cbPaletteIndex->palette[3] = readPalette(ppu, paletteIndex + 3);
-				// Graphics2D::SetPSConstantBuffer(2, cbPaletteIndex);
-				(void)patternTable(id * tile_8, 0, tile_8, tile_8).draw(Point{x, y} * tile_8, color);
-			}
-		}
+		// for (int16 y = 0; y < h_30; ++y)
+		// {
+		// 	for (int16 x = 0; x < w_32; ++x)
+		// 	{
+		// 		const uint16 addr = x + y * 32;
+		// 		const uint8 id = ppu.m_nametableData[addr]; // mmu.ReadChr8(addr);
+		// 		const uint8 attrIndex = 0x3C0 | (addr & 0xC00) | ((addr >> 4) & 0x38) | ((addr >> 2) & 0x7);
+		// 		const uint8 attribute = ppu.m_nametableData[attrIndex];
+		// 		const uint8 shift = ((addr >> 4) & 4) | (addr & 2);
+		// 		const uint8 paletteIndex = ((attribute >> shift) & 0x3) << 2;
+		// 		ColorF color{};
+		// 		color.r = readPalette(ppu, paletteIndex + 0) / 64.0;
+		// 		color.g = readPalette(ppu, paletteIndex + 1) / 64.0;
+		// 		color.b = readPalette(ppu, paletteIndex + 2) / 64.0;
+		// 		color.a = readPalette(ppu, paletteIndex + 3) / 64.0;
+		// 		(void)patternTable(id * tile_8, 0, tile_8, tile_8).draw(Point{x, y} * tile_8, color);
+		// 	}
+		// }
+		(void)patternTable.resized(Display_256x240).draw();
 
 		profiler.end(U"BG");
 		profiler.console();
